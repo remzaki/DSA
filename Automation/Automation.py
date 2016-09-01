f99e96d2ff04047a8f25709345d47a91c9c95e27
remzaki <reimark.cogonon@gmail.com>
2016-09-01 19:25:51 +0800

    Adjusted the test generation for the new parser

--- Automation/Automation.py
+++ Automation/Automation.py
@@ -25,16 +25,16 @@ class TestSequenceMeta(type):
     """
 
     def __new__(mcs, name, bases, dict):
-        def gen_test(datas):
-            def test(self):
+        def gen_test(testdatas):
+            def gen(self):
                 log_name = '%s-%s' % (self.__name__, self.desired_capabilities['browserName'])
                 logger.setup_logger(log_name, '%s.log' % log_name, level=logging.DEBUG)
                 log = logging.getLogger(log_name)
                 log.info('Executing Test: %s', self.__name__)
 
-                self.assertTrue(datas is not None)
+                self.assertTrue(testdatas is not None)
                 actions = Actions()
-                for step, d in enumerate(datas):
+                for step, d in enumerate(testdatas):
                     cmd = d[0].lower()
                     param1 = d[1].replace('"', '')
                     param2 = d[2]
@@ -45,21 +45,28 @@ class TestSequenceMeta(type):
                     if step+1 == 93:
                         pass
 
-            return test
+            return gen
 
-        for i in data.items():
-            name = i[0].replace('\'', '').strip().replace(' ', '_')
-            test_name = name
-            if name.split('_', 1)[0] != 'test':
-                test_name = 'test_%s' % name
-            a = i[1]
-            # print(a)
-            dict[test_name] = gen_test(a)
+        for s, tests in data.items():
+            suite = s.replace('\'', '').strip().replace(' ', '_')
+            name = suite
+            # print '-'+suite
+            for n, test in tests.items():
+                test_name = n.replace('\'', '').strip().replace(' ', '_')
+                # print '--'+test_name
+                # print('---%s' % test)
+                if test_name.split('_', 1)[0] != 'test':
+                    test_name = 'test_%s' % test_name
+                dict[test_name] = gen_test(test)
+
         return type.__new__(mcs, name, bases, dict)
 
 
 @on_platforms(platforms)
 class TestClass(BaseTest):
+    for s, tests in data.items():
+        suite = s.replace('\'', '').strip().replace(' ', '_')
+
     @classmethod
     def setup_class(cls):
         BaseTest.setup_class()
@@ -89,5 +96,4 @@ class TestClass(BaseTest):
 
 
 if __name__ == '__main__':
-    # print config.parallel
     pytest.main(config.parallel)
