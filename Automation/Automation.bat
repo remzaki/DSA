d2faa49c7a879b5b462e219ed5bc7d2b811e9c4c
remzaki <reimark.cogonon@gmail.com>
2016-11-28 21:52:05 +0800

    Replace run.bat with Automation.bat with enhancements in calling the Automation.py

---
+++ Automation/Automation.bat
@@ -0,0 +1,7 @@
+@echo off
+
+REM Execute Automation
+C:\Python27\python.exe "Automation.py"
+
+REM Display Automation Report after
+.\report\report.html
--- Automation/run.bat
+++
@@ -1,4 +0,0 @@
-rem @echo off
-C:\Python27\python.exe "D:\DSA\Automation\Automation.py"
-rem pause
-D:\DSA\Automation\report\report.html
